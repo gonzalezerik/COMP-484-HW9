112845782888324fe81e524e0cdfbc3407acb193
Erik Gonzalez <60283763+gonzalezerik@users.noreply.github.com>
2026-04-07 19:13:34 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 var currentDate = new Date();
 
-var currentMonth = currentDate.getMonth() + 1; // Months are 0-indexed, so we add 1
+var currentMonth = currentDate.getMonth() + 1; // Months are 0-indexed, so we add 1 [cite: 1015]
 var currentDay = currentDate.getDate();
 var currentYear = currentDate.getFullYear();
 
-// Add leading zeros if the month or day is less than 10
+// Add leading zeros if the month or day is less than 10 [cite: 1015]
 
 if (currentMonth < 10) {
     currentMonth = "0" + currentMonth;
@@ -26,7 +26,7 @@ var strValTwo = "250";
 var decVal = "3.14";           
 var wordVal = "javascript";    
 
-// Convert, check, and create sentences for each variable
+// Convert, check, and create sentences for each variable [cite: 703, 746]
 // Number() object to convert the string
 var convertedOne = Number(strValOne);
 
@@ -42,7 +42,7 @@ var resultSentenceDecimal = "Original: '" + decVal + "' &rarr; Converted: " + co
 var convertedWord = Number(wordVal);
 var resultSentenceWord = "Original: '" + wordVal + "' &rarr; Converted: " + convertedWord + " &rarr; isNaN: " + Number.isNaN(convertedWord) + " &rarr; isInteger: " + Number.isInteger(convertedWord) + "<br>";
 
-// 1st if/else statement 
+// 1st if/else statement [cite: 850, 858]
 var wordCheckMsg = "";
 if (Number.isNaN(convertedWord)) {
     wordCheckMsg = "<p><em>Note: The value '" + wordVal + "' is not a valid number.</em></p>";
@@ -58,28 +58,31 @@ document.getElementById("numberConversionOutput").innerHTML = finalConversionOut
 
 // Part 3: Math & Formatting (Option C: Large numbers)
 
-
 var totalPopulation = 8100000000;
 var yearlyIncrease = 75000000; 
 var yearsToProject = 5;
 
-//Perform calculations
+//Perform calculations 
 
 var totalIncrease = yearlyIncrease * yearsToProject;
 var futurePopulation = totalPopulation + totalIncrease;
 
+// EXTRA CREDIT: Ternary Operator 
+// Syntax: condition ? trueValue : falseValue
+var growthStatus = yearlyIncrease > 0 ? "Positive" : "Negative";
+
 //toLocaleString() and build a string
-// += to  add onto the string
+// += to add onto the string
 var mathResults = "<p>Current Population: " + totalPopulation.toLocaleString() + "</p>";
-mathResults += "<p>Yearly Growth: " + yearlyIncrease.toLocaleString() + "</p>";
+mathResults += "<p>Yearly Growth: " + yearlyIncrease.toLocaleString() + " (" + growthStatus + ")</p>";
 mathResults += "<p>Increase over 5 years: " + totalIncrease.toLocaleString() + "</p>";
 mathResults += "<p><strong>Projected Population: " + futurePopulation.toLocaleString() + "</strong></p>";
 
-// 2nd if/else statement 
+// 2nd if/else statement [cite: 851, 859]
 if (futurePopulation > 8400000000) {
     mathResults += "<p><em>Message: This is a huge number!</em></p>";
 } else {
-    mathResults += "<p><em>Message: This is a small number!</em></p>";
+    mathResults += "<p><em>Message: it wasnt too bad!</em></p>";
 }
 
 //Display the results
