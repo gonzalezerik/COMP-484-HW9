50aa3eb094c0dc7a50c843b8a0c559f3b2e6a5c7
Erik Gonzalez <60283763+gonzalezerik@users.noreply.github.com>
2026-04-07 19:09:49 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -56,29 +56,28 @@ document.getElementById("numberConversionOutput").innerHTML = finalConversionOut
 
 
 
-// Part 3: Math & Formatting
+// Part 3: Math & Formatting (Option C: Large numbers)
 
 
-var mealCost = 45.50;
-var tipRate = 0.20; // 20% tip
-var taxRate = 0.08; // 8% tax
+var totalPopulation = 8100000000;
+var yearlyIncrease = 75000000; 
+var yearsToProject = 5;
 
 //Perform calculations
 
-var taxAmount = mealCost * taxRate;
-var tipAmount = mealCost * tipRate;
-var totalCost = mealCost + taxAmount + tipAmount;
+var totalIncrease = yearlyIncrease * yearsToProject;
+var futurePopulation = totalPopulation + totalIncrease;
 
-//toFixed(2) and build a string
+//toLocaleString() and build a string
 // += to  add onto the string
-var mathResults = "<p>Meal Cost: $" + mealCost.toFixed(2) + "</p>";
-mathResults += "<p>Tax (8%): $" + taxAmount.toFixed(2) + "</p>";
-mathResults += "<p>Tip (20%): $" + tipAmount.toFixed(2) + "</p>";
-mathResults += "<p><strong>Total Bill: $" + totalCost.toFixed(2) + "</strong></p>";
+var mathResults = "<p>Current Population: " + totalPopulation.toLocaleString() + "</p>";
+mathResults += "<p>Yearly Growth: " + yearlyIncrease.toLocaleString() + "</p>";
+mathResults += "<p>Increase over 5 years: " + totalIncrease.toLocaleString() + "</p>";
+mathResults += "<p><strong>Projected Population: " + futurePopulation.toLocaleString() + "</strong></p>";
 
 // 2nd if/else statement 
-if (totalCost > 50) {
-    mathResults += "<p><em>Message: This is expensive!</em></p>";
+if (futurePopulation > 8400000000) {
+    mathResults += "<p><em>Message: This is a huge number!</em></p>";
 } else {
     mathResults += "<p><em>Message: it wasnt too bad!</em></p>";
 }
